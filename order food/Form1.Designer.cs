8c26b9b0aeaf4437497c1676a886d4e98ec53c3a
satyajit vala <satyajitvala23@gmail.com>
2026-08-12 20:44:19 +0530

    intial commit

--- order food/Form1.Designer.cs
+++ order food/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.button4 = new System.Windows.Forms.Button();
             this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
@@ -51,9 +52,11 @@
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.panel4 = new System.Windows.Forms.Panel();
             this.button5 = new System.Windows.Forms.Button();
             this.label11 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.panel5 = new System.Windows.Forms.Panel();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.radioButton4 = new System.Windows.Forms.RadioButton();
@@ -71,9 +74,11 @@
             this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.label16 = new System.Windows.Forms.Label();
             this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
             this.panel7 = new System.Windows.Forms.Panel();
             this.button6 = new System.Windows.Forms.Button();
             this.label19 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.panel8 = new System.Windows.Forms.Panel();
             this.radioButton5 = new System.Windows.Forms.RadioButton();
             this.radioButton6 = new System.Windows.Forms.RadioButton();
@@ -91,13 +96,8 @@
             this.comboBox4 = new System.Windows.Forms.ComboBox();
             this.label24 = new System.Windows.Forms.Label();
             this.label25 = new System.Windows.Forms.Label();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.label26 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label18 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -113,7 +113,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(505, 9);
+            this.label1.Location = new System.Drawing.Point(374, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(236, 24);
             this.label1.TabIndex = 0;
@@ -159,7 +159,7 @@
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label4);
-            this.panel1.Location = new System.Drawing.Point(30, 78);
+            this.panel1.Location = new System.Drawing.Point(367, 36);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(270, 474);
             this.panel1.TabIndex = 3;
@@ -185,6 +185,20 @@
             this.label10.Size = new System.Drawing.Size(77, 13);
             this.label10.TabIndex = 12;
             this.label10.Text = "TOTAL AMT :-";
+            // 
+            // button1
+            // 
+            this.button1.Image = global::order_food.Properties.Resources.icons8_bill_30;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(18, 379);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 38);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "TOTAL";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // panel3
             // 
@@ -359,9 +373,22 @@
             this.label3.TabIndex = 0;
             this.label3.Text = "PUNJABI";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Image = global::order_food.Properties.Resources.WhatsApp_Image_2026_08_11_at_10_39_54_PM__3__removebg_preview;
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label4.Location = new System.Drawing.Point(178, 6);
+            this.label4.MaximumSize = new System.Drawing.Size(100, 100);
+            this.label4.MinimumSize = new System.Drawing.Size(70, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 70);
+            this.label4.TabIndex = 4;
+            // 
             // panel4
             // 
             this.panel4.Controls.Add(this.button5);
+            this.panel4.Controls.Add(this.panel7);
             this.panel4.Controls.Add(this.label11);
             this.panel4.Controls.Add(this.button2);
             this.panel4.Controls.Add(this.panel5);
@@ -375,7 +402,7 @@
             this.panel4.Controls.Add(this.label16);
             this.panel4.Controls.Add(this.label17);
             this.panel4.Controls.Add(this.label18);
-            this.panel4.Location = new System.Drawing.Point(498, 78);
+            this.panel4.Location = new System.Drawing.Point(367, 36);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(270, 474);
             this.panel4.TabIndex = 4;
@@ -385,7 +412,7 @@
             // 
             this.button5.Location = new System.Drawing.Point(146, 379);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 38);
+            this.button5.Size = new System.Drawing.Size(84, 38);
             this.button5.TabIndex = 13;
             this.button5.Text = "CLEAR";
             this.button5.UseVisualStyleBackColor = true;
@@ -399,6 +426,20 @@
             this.label11.Size = new System.Drawing.Size(77, 13);
             this.label11.TabIndex = 12;
             this.label11.Text = "TOTAL AMT :-";
+            // 
+            // button2
+            // 
+            this.button2.Image = global::order_food.Properties.Resources.icons8_bill_30;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(33, 379);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 38);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "TOTAL";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // panel5
             // 
@@ -569,6 +610,18 @@
             this.label17.TabIndex = 0;
             this.label17.Text = "CHINESE";
             // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Image = global::order_food.Properties.Resources.icons8_noodles_70;
+            this.label18.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label18.Location = new System.Drawing.Point(178, 6);
+            this.label18.MaximumSize = new System.Drawing.Size(100, 100);
+            this.label18.MinimumSize = new System.Drawing.Size(70, 70);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(70, 70);
+            this.label18.TabIndex = 4;
+            // 
             // panel7
             // 
             this.panel7.Controls.Add(this.button6);
@@ -585,7 +638,7 @@
             this.panel7.Controls.Add(this.label24);
             this.panel7.Controls.Add(this.label25);
             this.panel7.Controls.Add(this.label26);
-            this.panel7.Location = new System.Drawing.Point(924, 78);
+            this.panel7.Location = new System.Drawing.Point(0, 0);
             this.panel7.Name = "panel7";
             this.panel7.Size = new System.Drawing.Size(270, 474);
             this.panel7.TabIndex = 13;
@@ -593,9 +646,9 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(155, 379);
+            this.button6.Location = new System.Drawing.Point(146, 379);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 38);
+            this.button6.Size = new System.Drawing.Size(84, 38);
             this.button6.TabIndex = 13;
             this.button6.Text = "CLEAR";
             this.button6.UseVisualStyleBackColor = true;
@@ -609,6 +662,20 @@
             this.label19.Size = new System.Drawing.Size(77, 13);
             this.label19.TabIndex = 12;
             this.label19.Text = "TOTAL AMT :-";
+            // 
+            // button3
+            // 
+            this.button3.Image = global::order_food.Properties.Resources.icons8_bill_30;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(33, 379);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(82, 38);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "TOTAL";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // panel8
             // 
@@ -778,33 +845,6 @@
             this.label25.TabIndex = 0;
             this.label25.Text = "FAST FOOD";
             // 
-            // button7
-            // 
-            this.button7.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button7.Image = global::order_food.Properties.Resources.icons8_exit_40;
-            this.button7.Location = new System.Drawing.Point(591, 576);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(93, 46);
-            this.button7.TabIndex = 14;
-            this.button7.Text = "EXIT";
-            this.button7.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
-            // 
-            // button3
-            // 
-            this.button3.Image = global::order_food.Properties.Resources.icons8_bill_30;
-            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button3.Location = new System.Drawing.Point(33, 379);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(82, 38);
-            this.button3.TabIndex = 11;
-            this.button3.Text = "TOTAL";
-            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // label26
             // 
             this.label26.AutoSize = true;
@@ -817,65 +857,25 @@
             this.label26.Size = new System.Drawing.Size(70, 70);
             this.label26.TabIndex = 4;
             // 
-            // button2
+            // button7
             // 
-            this.button2.Image = global::order_food.Properties.Resources.icons8_bill_30;
-            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button2.Location = new System.Drawing.Point(33, 379);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(82, 38);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "TOTAL";
-            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // label18
-            // 
-            this.label18.AutoSize = true;
-            this.label18.Image = global::order_food.Properties.Resources.icons8_noodles_70;
-            this.label18.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.label18.Location = new System.Drawing.Point(178, 6);
-            this.label18.MaximumSize = new System.Drawing.Size(100, 100);
-            this.label18.MinimumSize = new System.Drawing.Size(70, 70);
-            this.label18.Name = "label18";
-            this.label18.Size = new System.Drawing.Size(70, 70);
-            this.label18.TabIndex = 4;
-            // 
-            // button1
-            // 
-            this.button1.Image = global::order_food.Properties.Resources.icons8_bill_30;
-            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(18, 379);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(82, 38);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "TOTAL";
-            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Image = global::order_food.Properties.Resources.WhatsApp_Image_2026_08_11_at_10_39_54_PM__3__removebg_preview;
-            this.label4.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.label4.Location = new System.Drawing.Point(178, 6);
-            this.label4.MaximumSize = new System.Drawing.Size(100, 100);
-            this.label4.MinimumSize = new System.Drawing.Size(70, 70);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(70, 70);
-            this.label4.TabIndex = 4;
+            this.button7.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Image = global::order_food.Properties.Resources.icons8_exit_40;
+            this.button7.Location = new System.Drawing.Point(454, 518);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(93, 43);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "EXIT";
+            this.button7.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1300, 634);
+            this.ClientSize = new System.Drawing.Size(956, 570);
             this.Controls.Add(this.button7);
-            this.Controls.Add(this.panel7);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label2);
